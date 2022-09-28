ec6136eaff720e3a4c1727bfb9be2f54a8a61bca
Cyrius <me@cyri.us>
2022-09-28 16:07:07 +0200

    Add env var for configuration

--- build/src/entry.sh
+++ build/src/entry.sh
@@ -1,3 +1,8 @@
+replace() {
+  echo "replacing $1 by $2 in $3"
+  sed -i "s/$1/$2/g" $3
+}
+
 if [ -z "$UUID" ]; then
   UUID=1000
 fi
@@ -16,5 +21,14 @@ fi
 ln -s /dev/stdout /data/supervisord.log
 chmod 777 /data/supervisord.log
 chown -R $UUID:$GUID /data
+
+replace '$UUID' "$UUID" "/data/conf/dnsmasq.conf"
+replace '$GUID' "$GUID" "/data/conf/dnsmasq.conf"
+replace '$DHCP_START_ADDRESS' "$DHCP_START_ADDRESS" "/data/conf/dnsmasq.conf"
+replace '$DHCP_END_ADDRESS' "$DHCP_END_ADDRESS" "/data/conf/dnsmasq.conf"
+replace '$DHCP_ADDRESS_MASK' "$DHCP_ADDRESS_MASK" "/data/conf/dnsmasq.conf"
+replace '$DHCP_LEASE_TIME' "$DHCP_LEASE_TIME" "/data/conf/dnsmasq.conf"
+replace '$DHCP_ROUTER_ADDRESS' "$DHCP_ROUTER_ADDRESS" "/data/conf/dnsmasq.conf"
+replace '$DHCP_DNS' "$DHCP_DNS" "/data/conf/dnsmasq.conf"
 
 /usr/bin/supervisord -c /data/supervisord.conf
